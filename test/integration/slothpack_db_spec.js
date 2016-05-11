fbf51bdb0dc195217aa75d457acdb014e1130804
bharding2 <b.harding@gmail.com>
2016-05-11 10:16:01 -0700

    adjusting spec/html

--- test/integration/slothpack_db_spec.js
+++ test/integration/slothpack_db_spec.js
@@ -2,8 +2,8 @@ describe('slothpack app', function() {
   it('should have a 2 way data binding', function() {
     browser.get('http://localhost:5000');
     element(by.model('slothfact')).sendKeys('three toes');
-    element(by.id('slothfact')).getText().then(function(text) {
-      expect(text).toEqual('hello world');
+    element(by.binding('slothfact')).getText().then(function(text) {
+      expect(text).toEqual('three toes');
     });
   });
 });
